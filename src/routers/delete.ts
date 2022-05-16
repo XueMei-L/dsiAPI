a77fccb67e5427c8a92dbd94f3528f83eb21bcb2
Andrea Hernandez <alu0101119137@ull.edu.es>
2022-05-16 11:26:09 +0000

    realizado metodo patch y delete de artista

---
+++ src/routers/delete.ts
@@ -0,0 +1,40 @@
+import express from 'express';
+import {Artist} from '../models/artista';
+
+export const deleteRouter = express.Router();
+
+/**
+ * Eliminar un artista mediante query string
+ */
+deleteRouter.delete('/artist', (req, res) => {
+  if (!req.query.name) {
+    res.status(400).send({
+      error: 'A name must be provided',
+    });
+  } else {
+    Artist.findOneAndDelete({name: req.query.name.toString()}).then((artist) => {
+      if (!artist) {
+        res.status(404).send();
+      } else {
+        res.send(artist);
+      }
+    }).catch(() => {
+      res.status(400).send();
+    });
+  }
+});
+
+/**
+ * Eliminar un artista mediante un parámetro
+ */
+deleteRouter.delete('/artist/:id', (req, res) => {
+  Artist.findByIdAndDelete(req.params.id).then((artist) => {
+    if (!artist) {
+      res.status(404).send();
+    } else {
+      res.send(artist);
+    }
+  }).catch(() => {
+    res.status(400).send();
+  });
+});
---
+++ src/routers/patch.ts
@@ -0,0 +1,68 @@
+import express from 'express';
+import {Artist} from '../models/artista';
+
+export const patchRouter = express.Router();
+
+/**
+ * Actualización de un artista mediante query string
+ */
+patchRouter.patch('/artist', (req, res) => {
+  if (!req.query.name) {
+    res.status(400).send({
+      error: 'A name must be provided',
+    });
+  } else {
+    const allowedUpdates = ['name', 'genre', 'songs', 'listeners'];
+    const actualUpdates = Object.keys(req.body);
+    const isValidUpdate =
+      actualUpdates.every((update) => allowedUpdates.includes(update));
+
+    if (!isValidUpdate) {
+      res.status(400).send({
+        error: 'Update is not permitted',
+      });
+    } else {
+      Artist.findOneAndUpdate({name: req.query.name.toString()}, req.body, {
+        new: true,
+        runValidators: true,
+      }).then((artist) => {
+        if (!artist) {
+          res.status(404).send();
+        } else {
+          res.send(artist);
+        }
+      }).catch((error) => {
+        res.status(400).send(error);
+      });
+    }
+  }
+});
+
+/**
+ * Actualización de un artista mediante un parámetro
+ */
+patchRouter.patch('/artist/:id', (req, res) => {
+  const allowedUpdates = ['name', 'genre', 'songs', 'listeners'];
+  const actualUpdates = Object.keys(req.body);
+  const isValidUpdate =
+      actualUpdates.every((update) => allowedUpdates.includes(update));
+
+  if (!isValidUpdate) {
+    res.status(400).send({
+      error: 'Update is not permitted',
+    });
+  } else {
+    Artist.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true,
+    }).then((artist) => {
+      if (!artist) {
+        res.status(404).send();
+      } else {
+        res.send(artist);
+      }
+    }).catch((error) => {
+      res.status(400).send(error);
+    });
+  }
+});
